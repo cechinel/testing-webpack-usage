a8858c96cced4a0e0a328e103b572d3682ef835a
Alexandre <alexandre@sead96-69.sead.ufsc.br>
2017-01-31 14:51:41 -0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: [
+                    ['es2015']
+                ]
+            }
+        }, {
+            test: /\.scss$/,
+            loaders: ["style-loader", "css-loader", "sass-loader"]
+        }]
+    }
+};
